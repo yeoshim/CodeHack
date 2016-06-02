e480c189701fe6783397a709714881a4a93b0927
Daniel Junghoon Kim <yeoshim@gmail.com>
2016-06-03 01:03:16 +0900

    mercy & endians

---
+++ test/Mercy.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Mercy {
+	private static Scanner sc;
+
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		int cases = sc.nextInt();
+
+		while (cases-- > 0) {
+			System.out.println("Hello Algospot!");
+		}
+	}
+
+}
---
+++ test/com/codehack/lecture/Endians.java
@@ -0,0 +1,38 @@
+package com.codehack.lecture;
+
+import java.util.Scanner;
+
+public class Endians {
+	private static Scanner sc;
+
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		int cases = sc.nextInt();
+
+		while (cases-- > 0) {
+			String hex = Long.toHexString( sc.nextLong() );
+			char[] hexStr = hex.toCharArray();
+			char[] hexStr2 = new char[8];
+			char[] reverse = new char[8];
+			
+			for (int i = 0; i < 8; i++) {
+				hexStr2[i] = '0';
+				reverse[i] = '0';
+			}
+			
+			for (int i = 0; i < hexStr.length; i++) {
+				hexStr2[8-hexStr.length+i] = hexStr[i];
+			}
+			
+			for (int i = 0; i < 4; i++) {
+				int idx = i*2;
+				if( hexStr2[idx] == '0' && hexStr2[idx+1] == '0' )	continue;
+				reverse[8-1-idx-1] = hexStr2[idx];		//	0->6	2->4	4->2	6->0
+				reverse[8-(idx+1)] = hexStr2[idx+1];		//	1->7	3->5	5->3	7->1
+			}
+			
+			System.out.println( Long.valueOf(new String(reverse), 16) );
+//			System.out.println( Integer.parseInt(new String(reverse), 16) );	//	just [0-9] not [a-f]
+		}
+	}
+}
